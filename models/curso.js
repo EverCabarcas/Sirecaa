fa51bcb4d476471aa0410a33e38928c00b068786
Ever Cabarcas <evercm95@hotmail.com>
2017-10-28 17:00:06 -0500

    make it better

--- models/curso.js
+++ models/curso.js
@@ -5,8 +5,8 @@ var schema = new Schema({
     id_asignatura: {type: String, required: true},
     grupo: {type: String, required: true},
     nombre: {type: String, required: true},
-    id_proyecto: {type: Schema.Types.ObjectId, ref: 'Proyecto_Docente', required: true},
-    id_area: {type: Schema.Types.ObjectId, ref: 'Area', required: true}
+    id_proyecto: {type: Schema.Types.ObjectId, ref: 'Proyecto_Docente', required: false},
+    id_area: {type: Schema.Types.ObjectId, ref: 'Area', required: false}
 });
 
 module.exports = mongoose.model('Asignatura', schema);
