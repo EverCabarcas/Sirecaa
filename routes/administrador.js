f87f8211a3481a3d1625b48d3ab83f7879c7f3fa
Ever Cabarcas <evercm95@hotmail.com>
2017-10-28 12:49:01 -0500

    make it better

--- routes/administrador.js
+++ routes/administrador.js
@@ -147,7 +147,7 @@ router.post('/asignaturasprograma', function (req, res, err) {
 
         // You can get all kinds of information about the HTTP response.
         var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-        var data = JSON.parse(this.responseText); // Returned data, e.g., an HTML document.
+        var data = JSON.stringify(this.responseText); // Returned data, e.g., an HTML document.
         if (status != 200) {
             return res.status(status).json({
                 message: 'Error de peticion: ' + status
