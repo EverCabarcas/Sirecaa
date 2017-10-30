44302c9f002487b48b57cf0924c23a8d7b564a2c
Ever Cabarcas <evercm95@hotmail.com>
2017-10-30 13:59:26 -0500

    arreglo de nuevo

--- routes/administrador.js
+++ routes/administrador.js
@@ -7,7 +7,6 @@ var Horario = require('../models/horario');
 var Tema = require('../models/tema');
 var date = new Date();
 var data2;
-var j;
 cur = '';
 var jwt = require('jsonwebtoken');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
@@ -172,7 +171,7 @@ router.post('/asignaturasprograma', function (req, res, next) {
                 }
             }
             if (count != 0){
-                for ( j = 0; j < data.length; j++) {
+                for ( var j = 0; j < data.length; j++) {
 
                     curso.find({
                         id_asignatura: data[j].id_asignatura,
