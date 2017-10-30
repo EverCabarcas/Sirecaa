f300ddb83a0f96531b13defbc22d74cbfbc74170
Ever Cabarcas <evercm95@hotmail.com>
2017-10-29 20:50:12 -0500

    cambios en la asignatura

--- routes/administrador.js
+++ routes/administrador.js
@@ -168,6 +168,9 @@ router.post('/asignaturasprograma', function (req, res, next) {
                    });
                }
            }
+            res.status(500).json({
+                message: data
+            });
                for( var j =0; j< data.length; j++) {
                    curso.find({id_asignatura: data[j].id_asignatura, grupo: data[j].grupo}, function (err, resultado) {
                        if(err){
