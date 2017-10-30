7217dd56db66e3d82d2ab622528c15fa96326b6c
Ever Cabarcas <evercm95@hotmail.com>
2017-10-30 18:28:13 -0500

    arreglo de nuevo

--- routes/administrador.js
+++ routes/administrador.js
@@ -175,8 +175,7 @@ router.post('/asignaturasprograma', function (req, res, next) {
                 var a;
                 for ( var j = 0; j < data.length; j++) {
                     curso.find({
-                        id_asignatura: 'IA08106',
-                        grupo: 'A1'
+                        id_asignatura: 'IA08106'
                     }, function(err,resultado) {
                         if (err) {
                             return res.status(500).json({
