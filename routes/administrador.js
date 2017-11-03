bc5576fa22a1511e8e7ab7e46c634463f722c350
Ever Cabarcas <evercm95@hotmail.com>
2017-11-03 17:40:55 -0500

    Arreglando fechas

--- routes/administrador.js
+++ routes/administrador.js
@@ -108,13 +108,17 @@ router.post('/proyectodocenteinfo', function (req, res, next) {
 });
 
 router.post('/proyectodocente', function (req, res, next) {
+    var fecha2 = date.getTime() + (184*24*60*60*1000);
+    var fecha6 = new Date(fecha2);
+    // Milisegundos de 3 días mas suma3dias= 30*24*60*60*1000; (días * 24 horas * 60 minutos * 60 segundos * 1000 milésimas de segundo)
+
     var proyecto = new proyecto_docente({
         id_programa: req.body.id_programa,
         objetivos: req.body.objetivos,
         competencias: req.body.competencias,
         bibliografia: req.body.bibliografia,
-        fecha_inicio: ''+(date.getDay())+'/'+(date.getMonth()+1)+'/'+date.getFullYear(), // milisegundos
-        fecha_fin: ''+(date.getDay())+'/'+((date.getMonth()+1)+6)+'/'+date.getFullYear()
+        fecha_inicio: date.getTime(), // milisegundos ''+(date.getDay())+'/'+(date.getMonth()+1)+'/'+date.getFullYear()
+        fecha_fin:    fecha6.getTime()               //''+(date.getDay())+'/'+((date.getMonth()+1)+6)+'/'+date.getFullYear()
     });
     proyecto.save(function (err, result) {
         if (err) {
